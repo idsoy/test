16ed46d33c22425e3e05e35c98262815cefc95d3
idsoy <suncallh@idsoy.com>
2016-05-20 14:17:02 +0800

    add(test): test double2bigdecimal
    test double2bigdecimal
    
    close ##

---
+++ src/main/java/org/idsoy/test/algorithm/Double2BigDecimal.java
@@ -0,0 +1,17 @@
+package org.idsoy.test.algorithm;
+
+import java.math.BigDecimal;
+
+public class Double2BigDecimal {
+
+	public static void main(String[] args) {
+		
+		Double point = 1.90;
+		
+		BigDecimal bPoint = new BigDecimal(point);
+		
+		System.out.println(bPoint);
+		
+	}
+	
+}
